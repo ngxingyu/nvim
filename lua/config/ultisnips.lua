23e25d396a77654a4d7ba1b442e6b2f74c36280b
ngxingyu <ng.xingyu@dhs.sg>
2021-07-08 20:21:20 +0800

    update tex snippets

---
+++ lua/config/ultisnips.lua
@@ -0,0 +1,11 @@
+vim.cmd('let g:UltiSnipsSnippetDirectories=["UltiSnips"]')
+-- vim.api.nvim_set_var("UltiSnipsExpandTrigger",'<c-m>')
+-- vim.api.nvim_set_var("UltiSnipsJumpForwardTrigger",'<c-m>')
+-- vim.api.nvim_set_var("UltiSnipsJumpBackwardTrigger",'<c-n>')
+-- vim.g.UltiSnipsExpandTrigger = "<tab>"      
+-- vim.g.UltiSnipsJumpForwardTrigger = "<C-j>" 
+-- vim.g.UltiSnipsJumpBackwardTrigger = "<C-k>"
+vim.g.UltiSnipsExpandTrigger = "<NUL>"
+vim.g.UltiSnipsListSnippets = "<NUL>"
+vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<s-tab>"
